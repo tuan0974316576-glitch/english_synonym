04e139052bb43a7c6b1d9a31f50f5944420633ae
tuan0974316576-glitch <tuan0974316576@gmail.com>
2026-04-06 01:35:15 +0800

    Add install prompts and service worker shell

---
+++ sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', () => {
+  // Lightweight service worker for installability and future push support.
+});
